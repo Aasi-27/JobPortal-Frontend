7b37ea238ed5a801e0d499e9e1ea43e36c7352e7
Aasi-27 <167693237+Aasi-27@users.noreply.github.com>
2026-06-08 15:53:18 +0530

    Update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://job-portal-backend-production-b9a4.up.railway.app',
 });
 
 API.interceptors.request.use((config) => {
